6200d5c64bd44cf31538c96de54bca78d6f9cc01
Andre-Faustino <andre.domingues.faustino@hotmail.com>
2021-06-09 21:09:31 -0300

    Fase 2

--- hr-api-gateway-zuul/src/main/java/com/ms/hrapigatewayzuul/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/com/ms/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -1,15 +1,17 @@
-package com.ms.hrworker;
+package com.ms.hrapigatewayzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableEurekaClient
+@EnableZuulProxy
 @SpringBootApplication
-public class HrWorkerApplication {
+public class HrApiGatewayZuulApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HrWorkerApplication.class, args);
+		SpringApplication.run(HrApiGatewayZuulApplication.class, args);
 	}
 
 }
