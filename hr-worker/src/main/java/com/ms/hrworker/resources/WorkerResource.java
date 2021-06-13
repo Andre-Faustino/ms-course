a85153b4e8b0eee1a6a0e0fb1a423521b11d9837
Andre-Faustino <andre.domingues.faustino@hotmail.com>
2021-06-13 16:43:14 -0300

    Fase 3

--- hr-worker/src/main/java/com/ms/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/ms/hrworker/resources/WorkerResource.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,7 @@ import com.ms.hrworker.entities.Worker;
 import com.ms.hrworker.repositories.WorkerRepository;
 
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
@@ -27,6 +30,15 @@ public class WorkerResource {
 	
 	@Autowired
 	private WorkerRepository repo;
+	
+	@Value("${test.config}")
+	private String testConfig;
+	
+	@GetMapping(value = "/configs")
+	public ResponseEntity<Void> getConfig(){
+		logger.info("CONFIG = " + testConfig);
+		return ResponseEntity.noContent().build();
+	}
 	
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll(){
